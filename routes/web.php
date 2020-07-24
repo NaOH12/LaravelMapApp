d8c61c7a711d9273b7088d0dbefa3f27491a2ae0
Noah <964270@swansea.ac.uk>
2020-07-24 01:02:17 +0100

    Added a construction page

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('unfinished');
 });
 
-// Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
