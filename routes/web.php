bb3e06fc131c37b458e3d3736d987b66bc32b4b2
Noah Lenagan <noah.lenagan2175@gmail.com>
2020-08-06 14:23:02 +0100

    Yes.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Portfolio/index.html');
+    return view('unfinished');
 });
 
 // Route::get('/home', 'HomeController@index')->name('home');
